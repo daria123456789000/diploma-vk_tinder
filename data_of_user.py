4b9e0b9deb6758f9acd865bfd8da4364feeeaccc
daria123456789000 <129685028+daria123456789000@users.noreply.github.com>
2023-07-26 01:29:53 +0300

    Create data_of_user.py

---
+++ data_of_user.py
@@ -0,0 +1,73 @@
+if __name__ == '__main__':
+        if msg_text == "vkinder":
+            menu_bot(user_id)
+            msg_text, user_id = loop_bot()
+        
+            if msg_text.lower() == 'да':
+                reg_new_user(user_id)
+            elif len(msg_text) > 1:
+                sex = 0
+                if msg_text[0:7].lower() == 'девушка':
+                    sex = 1
+                elif msg_text[0:7].lower() == 'мужчина':
+                    sex = 2
+                age_at = msg_text[8:10]
+                if int(age_at) < 18:
+                    write_msg(user_id, 'Показан минимальный возраст - 18 лет')
+                    age_at = 18
+                age_to = msg_text[11:14]
+                if int(age_to) >= 100:
+                    write_msg(user_id, 'Показано максимальное значение 99 лет')
+                    age_to = 99
+                city = msg_text[14:len(msg_text)].lower()
+              result = search_users(sex, int(age_at), int(age_to), city)
+                create_result(result)
+                current_user_id = check_db_master(user_id)
+                for i in range(len(result)):
+                    dating_user, blocked_user = check_db_user(result[i][3])
+                   user_photo = get_profile_photos(result[i][3])
+                    if user_photo == 'нет доступа к фото' or dating_user is not None or blocked_user is not None:
+                        continue
+                    sorted_user_photo = sort_photos_by_likes(user_photos)
+                    write_msg(user_id, f'\n{result[i][0]}  {result[i][1]}  {result[i][2]}', )
+                    try:
+                        write_msg(user_id, f'фото:',
+                                  attachment=','.join
+                                  ([sorted_user_photo[-1][1], sorted_user_photo[-2][1],
+                                    sorted_user_photo[-3][1]]))
+                    except IndexError:
+                        for photo in range(len(sorted_user_photo)):
+                            write_msg(user_id, f'фото:',
+                                      attachment=sorted_user_photo[photo][1])
+
+
+def user_blacklist(id):
+    all_users = check_db_black(id)
+    write_msg(id, f'Анкеты в черном списке:')
+    for num, user in enumerate(all_users):
+        write_msg(id, f'{user.name}, {user.surname}, {user.link}')
+        write_msg(id, '1 - Удалить из черного списка, 0 - Далее \nq - Выход')
+        msg_texts, user_id = loop_bot()
+        if msg_texts == '0':
+            if num >= len(all_users) - 1:
+                write_msg(user_ids, f'Это была последняя анкета.\n'
+                                    f'Vkinder - вернуться в меню\n')
+        elif msg_texts == '1':
+            print(user.id)
+            delete_user_from_blacklist(user.vk_id)
+            write_msg(user_id, f'Анкета успешно удалена')
+            if num >= len(all_users) - 1:
+                write_msg(user_ids, f'Это была последняя анкета.\n'
+                                    f'Vkinder - вернуться в меню\n')
+
+
+
+    if not user_list:
+        vk.send_message(vk.vk_group_session, self_id, "Тебе пары не нашлось, попробуй скорректировать свои "
+                                                      "данные.\nМожет ты не указал свой город в личном профиле?",
+                        keyboard=welcome_keyboard.get_keyboard())
+        user_dict[self_id] = 1
+        return
+       elif msg_texts.lower() == 'q':
+            write_msg(id, 'Vkinder для активации бота')
+            break
