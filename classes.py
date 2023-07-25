a72c5c743b31ff9fe21c161119f57c7825b9572e
daria123456789000 <129685028+daria123456789000@users.noreply.github.com>
2023-07-26 01:42:14 +0300

    Create classes.py

---
+++ classes.py
@@ -0,0 +1,40 @@
+# Пользователь бота ВК
+class User(Base):
+    __tablename__ = 'user'
+    id = sq.Column(sq.Integer, primary_key=True, autoincrement=True)
+    vk_id = sq.Column(sq.Integer, unique=True)
+
+
+# Анкеты добавленные в избранное
+class Fav_User(Base):
+    __tablename__ = 'dating_user'
+    id = sq.Column(sq.Integer, primary_key=True, autoincrement=True)
+    vk_id = sq.Column(sq.Integer, unique=True)
+    first_name = sq.Column(sq.String)
+    second_name = sq.Column(sq.String)
+    city = sq.Column(sq.String)
+    link = sq.Column(sq.String)
+    id_user = sq.Column(sq.Integer, sq.ForeignKey('user.id', ondelete='CASCADE'))
+
+# Анкеты в черном списке
+class BlackList(Base):
+    __tablename__ = 'black_list'
+    id = sq.Column(sq.Integer, primary_key=True, autoincrement=True)
+    vk_id = sq.Column(sq.Integer, unique=True)
+    first_name = sq.Column(sq.String)
+    second_name = sq.Column(sq.String)
+    city = sq.Column(sq.String)
+    link = sq.Column(sq.String)
+    link_photo = sq.Column(sq.String)
+    count_likes = sq.Column(sq.Integer)
+    id_user = sq.Column(sq.Integer, sq.ForeignKey('user.id', ondelete='CASCADE'))
+    
+# Фото избранных анкет
+class Photos(Base):
+    __tablename__ = 'photos'
+    id = sq.Column(sq.Integer, primary_key=True, autoincrement=True)
+    link_photo = sq.Column(sq.String)
+    count_likes = sq.Column(sq.Integer)
+    id_dating_user = sq.Column(sq.Integer, sq.ForeignKey('dating_user.id', ondelete='CASCADE'))
+
+
