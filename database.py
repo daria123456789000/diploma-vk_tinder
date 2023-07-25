4e1799670d1875c53772cfac7958edb2370241be
daria123456789000 <129685028+daria123456789000@users.noreply.github.com>
2023-07-26 01:43:46 +0300

    Create database.py

---
+++ database.py
@@ -0,0 +1,101 @@
+# Удаляет пользователя из черного списка
+def delete_user_from_blacklist(ids):
+    current_user = session.query(BlackList).filter_by(vk_id=ids).first()
+    session.delete(current_user)
+    session.commit()
+
+
+# Удаляет пользователя из избранного
+def delete_user_from_favorites(ids):
+    current_user = session.query(DatingUser).filter_by(vk_id=ids).first()
+    session.delete(current_user)
+    session.commit()
+
+
+# Проверяет, зарегистрирован ли пользователь бота в БД
+def check_db_master(ids):
+    current_user_id = session.query(User).filter_by(vk_id=ids).first()
+    return current_user_id
+
+
+# Проверяет, есть ли пользователь в БД
+def check_db_user(ids):
+    dating_user = session.query(DatingUser).filter_by(vk_id=ids).first()
+    blocked_user = session.query(BlackList).filter_by(vk_id=ids).first()
+    return dating_user, blocked_user
+
+
+# Проверяет, есть ли пользователь в черном списке
+def check_db_black(ids):
+    current_users_id = session.query(User).filter_by(vk_id=ids).first()
+    # Находим все анкеты из избранного, которые добавил данный пользователь
+    all_users = session.query(BlackList).filter_by(user_id=current_users_id.id).all()
+    return all_users
+
+
+# Проверяет, есть ли пользователь в избранном
+def check_db_favorites(ids):
+    current_users_id = session.query(User).filter_by(vk_id=ids).first()
+    # Находим все анкеты из избранного, которые добавил данный пользователь
+    all_users = session.query(DatingUser).filter_by(user_id=current_users_id.id).all()
+    return all_users
+
+
+# Пишет сообщение пользователю
+def write_msg(user_id, message, attachment=None):
+    vk.method('messages.send',
+              {'user_id': user_id,
+               'message': message,
+               'random_id': vk_api.utils.get_random_id(),
+               'attachment': attachment})
+
+
+# Регистрация пользователя
+def register_user(vk_id):
+    try:
+        new_user = User(vk_id=vk_id)
+        session.add(new_user)
+        session.commit()
+        return True
+    except (IntegrityError, InvalidRequestError):
+        return False
+
+
+# Сохранение выбранного пользователя в БД
+def add_user_to_favorites(event_id, vk_id, first_name, second_name, city, link, id_user):
+    try:
+        new_user = DatingUser(
+            vk_id=vk_id,
+            first_name=first_name,
+            second_name=second_name,
+            city=city,
+            link=link,
+            id_user=id_user
+        )
+        session.add(new_user)
+        session.commit()
+        write_msg(event_id,
+                  'Пользователь успешно добавлен в избранное')
+        return True
+    except (IntegrityError, InvalidRequestError):
+        write_msg(event_id,
+                  'Пользователь уже в избранном.')
+        return False
+
+# Сохранение в БД фото добавленного пользователя
+def add_user_photos(event_id, link_photo, count_likes, id_dating_user):
+    try:
+        new_user = Photos(
+            link_photo=link_photo,
+            count_likes=count_likes,
+            id_dating_user=id_dating_user
+        )
+        session.add(new_user)
+        session.commit()
+        write_msg(event_id,
+                  'Фото пользователя сохранено в избранном')
+        return True
+    except (IntegrityError, InvalidRequestError):
+        write_msg(event_id,
+                  'Невозможно добавить фото этого пользователя(Уже сохранено)')
+        return False
